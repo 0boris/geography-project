5ef3717260b08ae56447b241d10db8166f0d6f34
0boris <145894897+0boris@users.noreply.github.com>
2025-07-16 11:42:20 +0000

    Fixed title.

--- src/lib/deepdives/Research.svelte
+++ src/lib/deepdives/Research.svelte
@@ -24,14 +24,14 @@
 
 <Deepdive on:scroll={(e) => (scrollProgress = e.detail)} class="" bind:isDeepDiveOpen={isDeepDiveResearchOpen}>
   <div class="fixed left-1/2 top-0 z-20 flex -translate-x-1/2 items-center justify-center">
-    <h2 class="bg-cen relative mt-6 w-full bg-[url('https://www.urbanexile.net/wordpress/wp-content/uploads/2015/03/Starfield_Instancing_white_uniform.png')] bg-cover bg-clip-text text-8xl font-bold text-transparent transition-colors delay-100 duration-300 group-hover:text-black md:text-9xl">Research</h2>
+    <h2 class="relative mt-6 w-full text-8xl font-bold transition-colors delay-100 duration-300 md:text-9xl">Research</h2>
   </div>
   <div class="relative flex w-full flex-shrink-0 backdrop-blur">
     <div class="relative mx-4 flex w-full items-center gap-10 md:mx-24 md:gap-24">
       <div class="relative z-20 max-w-xs text-white md:max-w-md lg:max-w-lg">
         <h3 class="mb-4 text-3xl font-bold">Understanding Water Scarcity</h3>
         <p class="text-xl leading-relaxed">This project took a long time to research. You can find the sources and bibliography in a word document, linked below.</p>
-        <a href="https://heckgrammarcouk-my.sharepoint.com/:w:/g/personal/23bosmanov_heckgrammar_co_uk/EedeujM_2_VBr9mn4i8vdaoBx-iJ3sMah8PDxgtmfAJ3Wg?e=U5Tm57" target="_blank" rel="noopener noreferrer" class="inline-block rounded-md bg-white px-5 py-1.5 text-base font-semibold text-black transition-all hover:bg-white/80 mt-8">Sources and Research</a>
+        <a href="https://heckgrammarcouk-my.sharepoint.com/:w:/g/personal/23bosmanov_heckgrammar_co_uk/EedeujM_2_VBr9mn4i8vdaoBx-iJ3sMah8PDxgtmfAJ3Wg?e=U5Tm57" target="_blank" rel="noopener noreferrer" class="inline-block rounded-md bg-white px-5 py-1.5 text-base font-semibold text-black transition-all hover:bg-white/70 mt-8">Sources and Research</a>
       </div>
 
       <VerticalCarousel id="melodysheep" text="All images are sourced from Google.">
